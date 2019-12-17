7cdad518cb221f9c1a7aef0f64a28e18a555c44c
b302-08 <b302-08@gmail.com>
2019-12-17 11:57:17 +0300

    u are gay

---
+++ test_Selskiy_Artem/test_Selskiy_Artem/Registration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_Selskiy_Artem
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Autorization Beh = new Autorization();
+            Beh.Show();
+        }
+    }
+}
